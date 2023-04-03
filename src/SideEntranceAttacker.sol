a5f746d0282c829656084cc91d4b373c04df75e5
Ratimon <ratixoxo@gmail.com>
2023-04-03 11:29:16 +0700

    Build(./src): Adding Unstoppable challenge#1

--- src/SideEntranceAttacker.sol
+++ src/SideEntranceAttacker.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.19;
 
-// import {console} from "@forge-std/console.sol";
-
 interface IChallenge {
     function deposit() external payable;
     function withdraw() external;
